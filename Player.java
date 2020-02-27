538b57169e5a529a9c19250a2b257731947c0cab
FlyingWolFox <lips.pissaia@gmail.com>
2020-02-27 18:14:52 -0300

    Add comments

--- Player.java
+++ Player.java
@@ -1,11 +1,22 @@
 import java.util.*;
 
+/**
+ * Representação do Jogador.
+ * 
+ * @author Lucas Isaac / lucaspissaia@cc.ci.ufpb.br
+ */
 public class Player {
 
-    private int number;
-    private ArrayList<Peca> pecas;
-    private String color;
+    private int number; // número da vez do jogador, 0 - 3
+    private ArrayList<Peca> pecas; // peças que correspondem ao jogador
+    private String color; // cor da guarita
 
+    /**
+     * Construtor. 
+     * @param number número do jogador
+     * @param casaDaGuarita casas da guarita do Jogador
+     * @param color cor da guarita do jogador 
+     */
     public Player(int number, Casa[] casaDaGuarita, String color) {
         this.number = number;
         this.color = color;
@@ -15,14 +26,27 @@ public class Player {
         }
     }
 
+    /**
+     * Número do jogador
+     * @return número do jogador
+     */
     public int getNumber() {
         return number;
     }
 
+    /**
+     * Cor do jogador
+     * @return cor do jogador
+     */
     public String getColor() {
         return color;
     }
 
+    /**
+     * Verifica se a peça passada pertence ao jogador
+     * @param peca peça a ser verificada
+     * @return true se a peça for do jogador, false caso contrário
+     */
     public boolean isThisPlayer(Peca peca) {
         return pecas.contains(peca);
     }
--- TurnManager.java
+++ TurnManager.java
@@ -1,15 +1,31 @@
 
+/**
+ * Gerenciador de turnos do jogo
+ * 
+ * @author Lucas Isaac
+ */
 public class TurnManager {
-    private int whoIsNow;
+    private int whoIsNow; // de quem é o turno
 
+    /**
+     * Construtor
+     * inicializa com jogador nº0 como primeiro
+     */
     public TurnManager() {
         whoIsNow = 0;
     }
 
+    /**
+     * Quem que joga agora
+     * @return numero do jogador que joga no turno
+     */
     public int getWhoIsNow() {
         return whoIsNow;
     }
 
+    /**
+     * Passa o turno para o próximo jogador
+     */
     public void next() {
         if (whoIsNow + 1 < 4)
             whoIsNow++;
